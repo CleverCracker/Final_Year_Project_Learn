12ef13d681bcf66ce8b5ef8fc3083ac474895392
Clever Cracker <clevercraker.gul@gmail.com>
2020-11-16 21:18:21 +0500

    ColorDetectionImport

--- ColorDetection/init.py
+++ ColorDetection/init.py
@@ -1,7 +1,15 @@
+import os
+from collections import Counter
+
 import cv2
-from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
-from collections import Counter
-from skimage.color import rgb2lab, deltaE_cie76
-import os
+from skimage.color import deltaE_cie76, rgb2lab
+from sklearn.cluster import KMeans
+
+# %matplotlib inline
+
+# Working with OpenCv
+color_detection_path = './ColorDetection/'
+load_iamge_path = 'room.jpg'
+image = cv2.imread(color_detection_path+load_iamge_path)
